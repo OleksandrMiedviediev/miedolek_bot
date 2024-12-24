1d353265c8d903854292280cf8371d88c90531b7
Oleksandr Miedviediev <medvedevbasket@gmail.com>
2024-12-24 07:07:09 +0100

    Delete telegram-webapp directory

--- telegram-webapp/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './App.jsx';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Добро пожаловать!/i);
-  expect(linkElement).toBeInTheDocument();
-});
